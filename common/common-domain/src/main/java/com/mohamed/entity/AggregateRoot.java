5aea9226a98ff18d5bb02e9a18d801fbf96e4939
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-03 11:36:56 +0300

    create new module for common domain entity code

---
+++ common/common-domain/src/main/java/com/mohamed/entity/AggregateRoot.java
@@ -0,0 +1,5 @@
+package com.mohamed.entity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+    // Additional methods and properties specific to aggregate roots can be added here
+}
