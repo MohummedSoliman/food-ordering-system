50d68fc4d466c9589fe391435c6d667471665d42
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-04 15:14:17 +0300

    create customer aggregate root domain class

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/mohamed/order/service/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package com.mohamed.order.service.domain.entity;
+
+import com.mohamed.entity.AggregateRoot;
+import com.mohamed.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+
+}
