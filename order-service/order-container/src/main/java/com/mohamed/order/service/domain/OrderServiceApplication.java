4175120b20d7728475aa8e3a0b5a9c390d2f043c
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-17 16:32:13 +0300

    add service class implementation as beans and init the spring context app

---
+++ order-service/order-container/src/main/java/com/mohamed/order/service/domain/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.mohamed.order.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@EnableJpaRepositories(basePackages = "com.mohamed.order.service.dataaccess")
+@EntityScan(basePackages = "com.mohamed.order.service.dataaccess")
+@SpringBootApplication(scanBasePackages = "com.mohamed")
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
