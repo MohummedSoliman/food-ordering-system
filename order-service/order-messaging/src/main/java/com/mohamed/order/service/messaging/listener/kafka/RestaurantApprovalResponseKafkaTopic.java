39798ff7c855bf1b6c34f1e3e900afe42d96896c
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-17 12:29:51 +0300

    implement listeners in kafka implementation

---
+++ order-service/order-messaging/src/main/java/com/mohamed/order/service/messaging/listener/kafka/RestaurantApprovalResponseKafkaTopic.java
@@ -0,0 +1,54 @@
+package com.mohamed.order.service.messaging.listener.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.OrderApprovalStatus;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalResponseAvroModel;
+import com.mohamed.kafka.consumer.KafkaConsumer;
+import com.mohamed.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalMessageResponseListener;
+import com.mohamed.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class RestaurantApprovalResponseKafkaTopic implements KafkaConsumer<RestaurantApprovalResponseAvroModel> {
+
+    private final RestaurantApprovalMessageResponseListener responseListener;
+    private final OrderMessagingDataMapper mapper;
+
+    public RestaurantApprovalResponseKafkaTopic(RestaurantApprovalMessageResponseListener responseListener,
+                                                OrderMessagingDataMapper mapper) {
+        this.responseListener = responseListener;
+        this.mapper = mapper;
+    }
+
+    @Override
+    @KafkaListener(id = "${kafka-consumer-config.restaurant-approval-consumer-group-id}",
+            topics = "${order-service.restaurant-approval-response-topic-name}")
+    public void receive(@Payload List<RestaurantApprovalResponseAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        log.info("{} number of payment responses received with key: {}, partitions: {} and offset: {}",
+                messages.size(), keys.toString(), partitions.toString(), offsets.toString());
+
+        messages.forEach(restaurantApprovalResponseAvroModel -> {
+            if (OrderApprovalStatus.APPROVED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
+                log.info("Processing approved order for order id: {}", restaurantApprovalResponseAvroModel.getOrderId());
+                responseListener.orderApproved(
+                        mapper.restaurantApprovalResponseArvoToRestaurantApprovalResponse(restaurantApprovalResponseAvroModel)
+                );
+            } else if (OrderApprovalStatus.REJECTED == restaurantApprovalResponseAvroModel.getOrderApprovalStatus()) {
+                log.info("Processing rejected order  for order id: {}", restaurantApprovalResponseAvroModel.getOrderId());
+                responseListener.orderRejected(
+                        mapper.restaurantApprovalResponseArvoToRestaurantApprovalResponse(restaurantApprovalResponseAvroModel)
+                );
+            }
+        });
+    }
+}
--- order-service/order-messaging/src/main/java/com/mohamed/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/mohamed/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -1,10 +1,14 @@
 package com.mohamed.order.service.messaging.mapper;
 
 import com.food.ordering.system.kafka.order.avro.model.*;
+import com.mohamed.order.service.domain.dto.message.PaymentResponse;
+import com.mohamed.order.service.domain.dto.message.RestaurantApprovalResponse;
 import com.mohamed.order.service.domain.entity.Order;
 import com.mohamed.order.service.domain.event.OrderCancelledEvent;
 import com.mohamed.order.service.domain.event.OrderCreatedEvent;
 import com.mohamed.order.service.domain.event.OrderPaidEvent;
+import com.mohamed.valueobject.OrderApprovalStatus;
+import com.mohamed.valueobject.PaymentStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -56,6 +60,35 @@ public class OrderMessagingDataMapper {
                         .collect(Collectors.toList()))
                 .setPrice(order.getPrice().getAmount())
                 .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
+                .build();
+    }
+
+    public PaymentResponse paymentResponseArvoModelToPaymentResponse(PaymentResponseAvroModel paymentResponseAvroModel) {
+        return PaymentResponse.builder()
+                .id(paymentResponseAvroModel.getId())
+                .orderId(paymentResponseAvroModel.getOrderId())
+                .paymentId(paymentResponseAvroModel.getPaymentId())
+                .customerId(paymentResponseAvroModel.getCustomerId())
+                .sagaId(paymentResponseAvroModel.getSagaId())
+                .price(paymentResponseAvroModel.getPrice())
+                .paymentStatus(PaymentStatus.valueOf(paymentResponseAvroModel.getPaymentStatus().toString()))
+                .createdAt(paymentResponseAvroModel.getCreatedAt())
+                .failureMessages(paymentResponseAvroModel.getFailureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalResponse restaurantApprovalResponseArvoToRestaurantApprovalResponse(
+            RestaurantApprovalResponseAvroModel responseAvroModel) {
+        return RestaurantApprovalResponse.builder()
+                .id(responseAvroModel.getId())
+                .orderId(responseAvroModel.getOrderId())
+                .restaurantId(responseAvroModel.getRestaurantId())
+                .sagaId(responseAvroModel.getSagaId())
+                .createdAt(responseAvroModel.getCreatedAt())
+                .failureMessages(responseAvroModel.getFailureMessages())
+                .orderApprovalStatus(
+                        OrderApprovalStatus.valueOf(responseAvroModel.getOrderApprovalStatus().toString())
+                )
                 .build();
     }
 }
