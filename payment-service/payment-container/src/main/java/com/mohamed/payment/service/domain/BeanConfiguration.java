3608910facd053a17152b94f5f348061632f137c
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-20 14:36:49 +0300

    complete payment container module

---
+++ payment-service/payment-container/src/main/java/com/mohamed/payment/service/domain/BeanConfiguration.java
@@ -0,0 +1,17 @@
+package com.mohamed.payment.service.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public PaymentDomainService paymentDomainService() {
+        return new PaymentDomainServiceImpl();
+    }
+
+
+
+
+}
---
+++ payment-service/payment-container/src/main/java/com/mohamed/payment/service/domain/PaymentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.mohamed.payment.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.mohamed.payment.service.dataaccess")
+@EntityScan(basePackages = "com.mohamed.payment.service.dataaccess")
+@SpringBootApplication(scanBasePackages = "com.mohamed")
+public class PaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
