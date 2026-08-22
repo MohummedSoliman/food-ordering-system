554e6a7e107f7bf55206d0c20342206e06f3406d
Mohamed Ibranim <mosuliman92@gmail.com>
2026-08-22 14:17:55 +0300

    complete restaurant container module implementation

--- restaurant-service/restaurant-container/src/main/java/com/mohamed/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/mohamed/restaurant/service/domain/RestaurantServiceApplication.java
@@ -1,4 +1,16 @@
 package com.mohamed.restaurant.service.domain;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = {"com.mohamed.restaurant.service.dataaccess", "com.mohamed.dataaccess"})
+@EntityScan(basePackages = {"com.mohamed.restaurant.service.dataacess", "com.mohamed.dataaccess"})
+@SpringBootApplication(scanBasePackages = "com.mohamed")
 public class RestaurantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
 }
